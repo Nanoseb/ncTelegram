2e14c7867e6be75f2a63c14729b0bdfd4b1447df
nanoseb <sebastien.lemaire@ecl13.ec-lyon.fr>
2016-01-31 17:08:47 +0000

    passage a ListBox

--- ui_msgwidget.py
+++ ui_msgwidget.py
@@ -2,14 +2,12 @@ import urwid
 import time
 
 # Le widget utilise pour afficher la liste des messages
-class MessageWidget(urwid.Filler):
+class MessageWidget(urwid.ListBox):
     def __init__(self):
         self.msgs = []
         self.updateLocked = False
-        
-        self.msg_list = urwid.Text(('msg', "Messages :"),align='left')
-        self.msg_list_attr = urwid.AttrWrap(self.msg_list, 'msg')
-        super().__init__(self.msg_list_attr, 'middle')
+        self.msg_list = urwid.SimpleFocusListWalker([urwid.Text(('msg', "Messages :"),align='left')]) 
+        super().__init__(self.msg_list)
     
     # Mettre a jour la liste des messages
     # msgList est une liste d'éléments type [auth, msg]
@@ -18,10 +16,10 @@ class MessageWidget(urwid.Filler):
         while (self.updateLocked):
             time.sleep(0.5)
         self.updateLocked = True
-        self.msg_list.set_text("msgs :")
+        pos = self.focus_position
         for i in msgList:
-            self.msg_list.set_text(self.msg_list.text 
-            + "\n" + i[0] + ": " + i[1])
+            self.msg_list.insert(pos + 1, urwid.Text(i[0]+": "+i[1]))
+            self.focus_position = pos + 1
         self.msgs = msgList
         self.updateLocked = False
 
