3a4445cd0f0b4b81b5c1568e67ccee103f44da83
0xf10413 <florent.gattoni@ec14.ec-lyon.fr>
2018-11-04 16:39:06 +0100

    Move content to libnctelegram
    Move classes in different modules. Put Telegram_ui in a dedicated module
    instead of __init__.py. Create a function to load configuration.
    
    nctelegram itself is now much smaller.

---
+++ ncTelegram/config.py
@@ -0,0 +1,119 @@
+#!/usr/bin/env python3
+"""
+Configuration class to handle the settings
+"""
+
+import os
+import sys
+import subprocess
+import psutil
+import os.path
+import configparser
+import logging
+
+logger = logging.getLogger(__name__)
+
+
+# TODO handle comand line argument:
+# -c configfile
+# --no-img to overwrite some parameters etc.
+
+def load_config():
+    """
+    Tries to find a config file, reads it, and return the config as a dict
+    """
+
+    # Load config file following the rule "local path" → "home" → "/etc"
+    confPriorityList = ['ncTelegram.conf',
+            os.path.expanduser("~")+'/.ncTelegram.conf',
+            '/etc/ncTelegram.conf']
+    for conffile in confPriorityList:
+        if os.path.isfile(conffile):
+            break
+    else:
+        raise Exception("Configuration not found in {},"
+              "please check your installation".format(confPriorityList))
+
+    config = configparser.ConfigParser()
+    config.read(conffile)
+
+    config_full = {}
+
+    config_full['general'] = {}
+    config_full['style'] = {}
+    config_full['keymap'] = {}
+
+    # General settings
+    settings = config['general']
+    config_full['general']['phone_number'] = settings.get('phone_number', None)
+    config_full['general']['user_login'] = settings.get('user_login', None)
+    config_full['general']['notification'] = settings.getboolean('notification', True)
+    config_full['general']['ninja_mode'] = settings.getboolean('ninja_mode', False)
+
+    config_full['general']['inline_image'] = settings.getboolean('inline_image', True)
+    config_full['general']['open_file'] = settings.getboolean('open_file', True)
+    config_full['general']['date_format'] = settings.get('date_format', "%x", raw=True)
+
+    # Style settings
+    settings = config['style']
+    config_full['style']['status_bar_fg'] = settings.get('status_bar_fg', 'bold, white')
+    config_full['style']['status_bar_bg'] = settings.get('status_bar_bg', 'dark gray')
+    config_full['style']['date'] = settings.get('date', 'light green')
+    config_full['style']['hour'] = settings.get('hour', 'dark gray')
+    config_full['style']['separator'] = settings.get('separator', 'dark gray')
+    config_full['style']['cur_chan'] = settings.get('cur_chan', 'light green')
+    config_full['style']['users_color'] = settings.get('users_color', 'dark red, '
+                                                                      'dark green, '
+                                                                      'brown, '
+                                                                      'dark blue, '
+                                                                      'dark magenta, '
+                                                                      'dark cyan, '
+                                                                      'light red, '
+                                                                      'light green, '
+                                                                      'yellow, '
+                                                                      'light blue, '
+                                                                      'light magenta, '
+                                                                      'light cyan, '
+                                                                      'white')
+    config_full['style']['user_color'] = settings.get('user_color', 'light green')
+
+    # Keymap settings
+    settings = config['keymap']
+    config_full['keymap']['left'] = settings.get('left', 'h')
+    config_full['keymap']['right'] = settings.get('right', 'l')
+    config_full['keymap']['up'] = settings.get('up', 'k')
+    config_full['keymap']['down'] = settings.get('down', 'j')
+    config_full['keymap']['quit'] = settings.get('quit', 'q')
+    config_full['keymap']['insert_text'] = settings.get('insert_text', 'i')
+    config_full['keymap']['open_file'] = settings.get('open_file', 'ctrl o')
+    config_full['keymap']['line_break'] = settings.get('line_break', 'ctrl r')
+    config_full['keymap']['next_chan'] = settings.get('next_chan', 'ctrl n')
+    config_full['keymap']['prev_chan'] = settings.get('prev_chan', 'ctrl p')
+    config_full['keymap']['hide_chanlist'] = settings.get('hide_chanlist', 'ctrl b')
+
+    ##  Extra checks, disable configuration if features unavailable
+    if not 'DISPLAY' in os.environ:
+        logger.warning("'DISPLAY' not found, disabling graphical features")
+        config_full['general']['notification'] = False
+        config_full['general']['open_file'] = False
+
+    if config_full['general']['notification']:
+        try:
+            import gi
+            gi.require_version('Notify', '0.7')
+            from gi.repository import Notify
+        except Exception:
+            logger.exception("Could not load gi, disabling notifications")
+            config_full['general']['notification'] = False
+
+    if config_full['general']['inline_image']:
+        try:
+            subprocess.call(['img2txt', '-h'],
+                    stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+        except Exception:
+            logger.exception("Could not call img2text, disabling inline images")
+            config_full['general']['inline_image'] = False
+
+    return config_full
+
+# vim: ai ts=4 sw=4 et sts=4
---
+++ ncTelegram/telegram_ui.py
@@ -0,0 +1,253 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import subprocess
+import time
+import logging
+
+import urwid
+
+# TODO something cleaner to he here?
+try:
+    import gi
+    gi.require_version('Notify', '0.7')
+    from gi.repository import Notify
+except:
+    pass
+
+from .ui_chanwidget import ChanWidget
+from .ui_msgwidget import MessageWidget
+from .ui_msgsendwidget import MessageSendWidget
+from .tg_client import TgClient
+
+logging.basicConfig(filename='logger.log', level=logging.DEBUG)
+logger = logging.getLogger(__name__)
+
+class Telegram_ui:
+    def __init__(self, conf):
+        # Boolean to prevent TgClient's to process updates
+        self.lock_receiver = True
+
+        # ncTelegram configuration as a dict
+        self.conf = conf
+
+        # Timestamp of start. Should be const
+        self.boot_time = int(time.time())
+
+        # Just shortcut for some configurations :
+        self.DATE_FORMAT = self.conf['general']['date_format']
+        self.NINJA_MODE = self.conf['general']['ninja_mode']
+        self.INLINE_IMAGE = self.conf['general']['inline_image']
+
+        # Actual telegram client, interacting with telegram's servers
+        self.tg_client = TgClient(self)
+
+        # ???
+        self.online_status = {}
+        # ???
+        self.read_status = {}
+
+        style = self.conf['style']
+        palette = [('status_bar', style['status_bar_fg'], style['status_bar_bg']),
+                   ('date', style['date'], ''),
+                   ('hour', style['hour'], ''),
+                   ('separator', style['separator'], ''),
+                   ('reversed', 'standout', ''),
+                   ('cur_chan', style['cur_chan'], ''),
+                   ('user_color', style['user_color'], '')
+                   ]
+
+        # Setup notifications if enabled
+        if self.conf['general']['notification']:
+            Notify.init("ncTelegram")
+            self.image = '/usr/share/ncTelegram/t_logo.png'
+
+        # ???
+        self.current_chan = {}
+        # ???
+        self.last_media = {}
+
+        # message buffer init
+        # msg_buffer is where incoming messages go before they have been printed
+        self.msg_buffer = {}
+
+        # msg_archive is where messages go once they are processed
+        # and have the proper layout (urwid list)
+        self.msg_archive = {}
+
+        # Shortcut to retrieve the client's entity faster
+        # TODO: might miss an update in profile ?
+        self.me = self.tg_client.get_me()
+
+
+        ## Create and setup UI
+        # List of dialogs on the left
+        self.chan_widget = ChanWidget(self)
+        self.print_title() # TODO: remove implicit dependency on chan_widget
+        # List of messages on the upper right
+        self.msg_widget = MessageWidget(self)
+        # Text area to write a message + status bar widget
+        self.msg_send_widget = MessageSendWidget(self)
+
+        # Right panel (list of messages + text area)
+        self.right_side = urwid.Pile([self.msg_widget, (2, self.msg_send_widget)])
+        vert_separator = urwid.AttrMap(urwid.Filler(urwid.Columns([])), 'status_bar')
+
+        # Final arrangements
+        self.main_columns = urwid.Columns([('weight', 1, self.chan_widget),
+                                           (1, vert_separator),
+                                           ('weight', 5, self.right_side)])
+
+        # urwid's main loop, responsible for drawing on the screen
+        self.main_loop = urwid.MainLoop(self.main_columns, palette,
+                unhandled_input=self.unhandle_key)
+        self.main_loop.screen.set_terminal_properties(colors=256)
+
+        # Allow tgclient to process updates, and to call the UI as it's now ready
+        self.lock_receiver = False
+
+        # TODO: use start() + a with: block
+        self.main_loop.run()
+
+    def update_online_status(self, when, status, cmd):
+        self.online_status[cmd] = (when, status)
+        if cmd == self.current_chan[1].id:
+            self.msg_send_widget.update_status_bar()
+
+    def update_read_status(self, cmd, bool):
+        self.read_status[cmd] = bool
+        if cmd == self.current_chan['id']:
+            self.msg_send_widget.update_status_bar()
+
+
+    def display_notif(self, msg):
+        if self.conf['general']['notification']:
+            text = msg['text']
+
+            if msg['receiver']['type'] == 'user':
+                sender = msg['sender']['first_name']
+            else:
+                sender = msg['receiver']['name'] + ": " + msg['sender']['first_name']
+
+
+            Notify.Notification.new('', '<b>' + sender + '</b>\n' + text, self.image).show()
+
+
+    def print_title(self):
+        total_msg_waiting = sum(self.chan_widget.msg_chan.values())
+        if total_msg_waiting == 0:
+            sys.stdout.write("\x1b]2;ncTelegram\x07")
+        else:
+            sys.stdout.write("\x1b]2;ncTelegram (" + str(total_msg_waiting) + ")\x07")
+
+
+    def fill_msg_buffer(self, button):
+
+        for chan in self.chan_widget.chans:
+            cmd = chan['id']
+            if cmd not in self.msg_buffer:
+                print_name = chan['print_name']
+                try:
+                    self.msg_buffer[cmd] = self.tg_client.history(print_name, 100)
+                except Exception as e:
+                    logger.warning("Got exception %s while filling msg buffer", e)
+                    self.msg_buffer[cmd] = []
+                if self.INLINE_IMAGE:
+                    for msg in self.msg_buffer[cmd]:
+                        if 'media' in msg:
+                            image = self.msg_widget.get_inline_img(msg)
+
+        self.chan_widget.update_chan_list()
+
+    def is_image(self, path):
+        # TODO is that needed? consider file as file if not send as photo is probably better
+        return not path == None and (path.endswith('png') \
+        or path.endswith('jpg') \
+        or path.endswith('jpeg') \
+        or path.endswith('JPG') \
+        or path.endswith('PNG'))
+
+
+    def download_media(self, msg):
+        if 'url' in msg:
+            return msg['url']
+        else:
+            mtype = msg['media']['type']
+            mid = msg['id']
+
+            #file = self.sender.load_file(mid)
+            if mtype == 'photo':
+                file = self.sender.load_photo(mid)
+
+            elif mtype == 'document':
+                file = self.sender.load_document(mid)
+            else:
+                file = None
+
+            return file
+
+    def open_file(self, path):
+        # TODO Not sure that's a good idea in the end, reconsider that, download file somewhere instead of opening it
+        if self.conf['general']['open_file'] and path != None:
+            subprocess.Popen(['xdg-open', path], stderr=subprocess.DEVNULL, stdout=subprocess.DEVNULL)
+
+
+
+
+    def stop_Telegram(self):
+        self.tg_client.disconnect()
+
+
+    def exit(self):
+        if self.conf['general']['notification']:
+            Notify.uninit()
+        # TODO: remove the comment (but it erases error messages)
+        #sys.stdout.write("\x1b]2;\x07")
+        self.stop_Telegram()
+        raise urwid.ExitMainLoop
+
+
+    def unhandle_key(self, key):
+        """
+        Function called whenever urwid did not handle a key
+        """
+        if key == self.conf['keymap']['quit']:
+            self.exit()
+
+        elif key == self.conf['keymap']['hide_chanlist']:
+
+            if 1 == self.main_columns.contents[0][1][1]:    # check if already hidden
+                # hidding
+                self.main_columns.contents[0] = (self.main_columns.contents[0][0],('given', 0, False) )
+                self.main_columns.contents[1] = (self.main_columns.contents[1][0],('given', 0, False) )
+            else:
+                self.main_columns.contents[0] = (self.main_columns.contents[0][0],('weight', 1, True) )
+                self.main_columns.contents[1] = (self.main_columns.contents[1][0],('given', 1, False) )
+
+
+        elif key == 'esc':
+            self.msg_widget.draw_separator()
+
+        elif key == self.conf['keymap']['prev_chan']:
+            self.chan_widget.go_prev_chan()
+
+        elif key == self.conf['keymap']['next_chan']:
+            self.chan_widget.go_next_chan()
+
+        elif key == self.conf['keymap']['open_file'] and \
+                self.last_media[self.current_chan['id']] != {} and \
+                        self.conf['general']['open_file']:
+             path = self.download_media(self.last_media[self.current_chan['id']])
+             self.open_file(path)
+
+        elif key == self.conf['keymap']['insert_text']:
+            self.main_columns.focus_position = 2
+            self.right_side.focus_position = 1
+
+        elif key == "'":
+            self.main_columns.focus_position = 2
+            self.right_side.focus_position = 1
+            self.msg_send_widget.widgetEdit.insert_text("'")
+
+# vim: ai ts=4 sw=4 et sts=4
